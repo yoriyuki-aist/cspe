3433335a5e9ac04b0318e62ee967b39b7d05e8bc
Yoriyuki Yamagata <yoriyuki.yamagata@aist.go.jp>
2016-08-09 18:38:31 +0900

    Security Example

---
+++ src/test/scala/jp/go/aist/cspe/SecurityExample.scala
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Copyright (c) 2014-2016. National Institute of Advanced Industrial Science and Technology (AIST)
+ *  * All rights reserved.
+ *
+ */
+
+package jp.go.aist.cspe
+import jp.go.aist.cspe.CSPE._
+import sun.security.krb5.Credentials
+
+/**
+  * Created by yoriyuki on 2016/08/08.
+  */
+object SecurityExample {
+
+  def authServer(credentials : Set[Int]) : Process = ?? {
+    case Event('Req, credential: Int) if ! credentials(credential) =>
+      authServer(credentials + credential) |||
+        Event('MakeResAvailable, credential) ->: Event('BecomeResAvailable, credential) ->:
+        Event('Granted, credential) ->: SKIP
+    case Event('Release, credential: Int) if credentials(credential) =>
+      authServer(credentials - credential) ||| Event('ReleaseRes, credential) ->: SKIP
+  }
+
+  def resources(credentials_asked: Set[Int], credentials_granted: Set[Int]) : Process = ?? {
+    case Event('MakeResAvailable, credential: Int) => resources(credentials_asked + credential, credentials_granted)
+    case Event('BecomeResAvailable, credential : Int) if credentials_asked(credential) =>
+      resources(credentials_asked - credential, credentials_granted + credential)
+    case Event('ReleaseRes, credential: Int) if credentials_granted(credential) =>
+        resources(credentials_asked, credentials_granted - credential)
+    case Event('Access, credential: Int) if credentials_granted(credential) =>
+        resources(credentials_asked, credentials_granted)
+  }
+
+  def client(pid: Int, credentials: Set[Int]): Process = ?? {
+    case Event('Spawn, `pid`, child_pid: Int) => client(pid, credentials) ||| client(child_pid, credentials)
+    case Event('Req, credential: Int) =>
+      Event('Granted, credential) ->: client(pid, credentials + credential)
+    case Event('Access, credential: Int) if credentials(credential) => client(pid, credentials)
+    case Event('Release, credential: Int) if credentials(credential) => client(pid, credentials - credential)
+  }
+
+  def server = (authServer(Set()) || Set('MakeResAvailable, 'BecomeResAvailable, 'ReleaseRes) || resources(Set(), Set())
+
+  def system = server || Set('Req, 'Granted, 'Access, 'Release) || client(0, Set())
+
+}
+
